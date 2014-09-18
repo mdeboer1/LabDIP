a8d6006688326fa6f300cdae6fcdb114ea907796
mdeboer1 <mdeboer1@L11108042787.wctc.msft>
2014-09-17 19:15:55 -0500

    Created new package in LabDIP, an interface, an enum, and two classes.  Moved common properties to the interface for inheritance purposes.

---
+++ src/mylab2/TipCalculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mylab2;
+
+/**
+ *
+ * @author mdeboer1
+ */
+public interface TipCalculator {
+    public static final double MIN_BILL = 0.00;
+    public static final double GOOD_RATE = 0.20;
+    public static final double FAIR_RATE = 0.15;
+    public static final double POOR_RATE = 0.10;
+    
+    
+    
+}
